e5e8168e748d818e98ba57513636a716fc0e4ed8
Cody Bloemhard <cblmhrd@live.nl>
2020-03-10 15:25:43 +0100

    docs n stuff

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+![Rust](https://github.com/ocdy1001/bin-buffer/workflows/Rust/badge.svg)
+[docs](https://docs.rs/bin_buffer/)
 # BinBuffer
 Simple lightweight crate for translating objects into binairy buffers.
+It supports reading and writing these types: u64, u32, u16, u8, f64, f32, (f64,f64), String.
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,22 @@
 use std::io::prelude::*;
 use std::fs::OpenOptions;
 
+/// # Example:
+/// ```
+/// use bin_buffer::*;
+/// let x = 16u16;
+/// let y = String::from("hello");
+/// let z = (0.0001f64,1.1111f64);
+/// let mut buffer = Vec::new();
+/// x.into_buffer(&mut buffer);
+/// y.copy_into_buffer(&mut buffer);
+/// z.into_buffer(&mut buffer);
+/// let mut buffer = ReadBuffer::from_raw(buffer);
+/// assert_eq!(x, u16::from_buffer(&mut buffer).unwrap());
+/// assert_eq!(y, String::from_buffer(&mut buffer).unwrap());
+/// assert_eq!(z, <(f64,f64)>::from_buffer(&mut buffer).unwrap());
+/// ```
+
 /// Buffer: a Vector of bytes
 pub type Buffer = Vec<u8>;
 
