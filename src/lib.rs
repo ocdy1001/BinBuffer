63690b2ca6f4e730db0396a63d763a3281c6c0fd
Cody Bloemhard <cblmhrd@live.nl>
2020-03-10 10:57:43 +0100

    strings Bufferable

--- src/lib.rs
+++ src/lib.rs
@@ -24,10 +24,10 @@ impl Bufferable for u32{
     fn from_buffer(vec: &Buffer, iter: &mut usize) -> Option<Self>{
         if *iter + 4 > vec.len() { return Option::None; }
         let mut val: u32 = 0;
-        val += u32::from(vec[(*iter + 0) as usize]) << 24;
-        val += u32::from(vec[(*iter + 1) as usize]) << 16;
-        val += u32::from(vec[(*iter + 2) as usize]) << 8;
-        val += u32::from(vec[(*iter + 3) as usize]);
+        val += u32::from(vec[(*iter + 0)]) << 24;
+        val += u32::from(vec[(*iter + 1)]) << 16;
+        val += u32::from(vec[(*iter + 2)]) << 8;
+        val += u32::from(vec[(*iter + 3)]);
         *iter += 4;
         Option::Some(val)
     }
@@ -46,8 +46,8 @@ impl Bufferable for u16{
     fn from_buffer(vec: &Buffer, iter: &mut usize) -> Option<Self>{
         if *iter + 2 > vec.len() { return Option::None; }
         let mut val: u16 = 0;
-        val += u16::from(vec[(*iter + 0) as usize]) << 8;
-        val += u16::from(vec[(*iter + 1) as usize]);
+        val += u16::from(vec[(*iter + 0)]) << 8;
+        val += u16::from(vec[(*iter + 1)]);
         *iter += 2;
         Option::Some(val)
     }
@@ -64,7 +64,7 @@ impl Bufferable for u8{
 
     fn from_buffer(vec: &Buffer, iter: &mut usize) -> Option<Self>{
         if *iter + 1 > vec.len() { return Option::None; }
-        let val = vec[*iter as usize];
+        let val = vec[*iter];
         *iter += 1;
         Option::Some(val)
     }
@@ -113,6 +113,36 @@ impl Bufferable for f32{
         }
         *iter += 4;
         return Option::Some(f32::from_be_bytes(bytes));
+    }
+}
+
+impl Bufferable for String{
+    fn into_buffer(self, vec: &mut Buffer){
+        self.copy_into_buffer(vec);
+    }
+
+    fn copy_into_buffer(&self, vec: &mut Buffer){
+        let bytes = self.as_bytes();
+        let len = bytes.len();
+        (len as u32).into_buffer(vec);
+        for b in bytes.iter(){
+            vec.push(*b);
+        }
+    }
+
+    fn from_buffer(vec: &Buffer, iter: &mut usize) -> Option<Self>{
+        let len = u32::from_buffer(vec, iter); //TODO: use usize
+        if len.is_none() { return Option::None; }
+        let len = len.unwrap() as usize;
+        if *iter + len > vec.len() { return Option::None; }
+        let mut bytes = Vec::new();
+        for i in 0..len{
+            bytes.push(vec[*iter + i]);
+        }
+        *iter += len;
+        let res = String::from_utf8(bytes);
+        if res.is_err() { return Option::None; }
+        return Some(res.unwrap());
     }
 }
 
@@ -210,5 +240,20 @@ mod tests{
         assert_eq!(x, f32::from_buffer(&buffer, &mut iter).unwrap());
         assert_eq!(y, f32::from_buffer(&buffer, &mut iter).unwrap());
         assert_eq!(Option::None, f32::from_buffer(&buffer, &mut iter));
+    }
+    #[test]
+    fn test_string(){
+        let x = String::from("haha yes cool and good");
+        let y = 16u16;
+        let z = String::from("another one");
+        let mut buffer = Vec::new();
+        x.copy_into_buffer(&mut buffer);
+        y.into_buffer(&mut buffer);
+        z.copy_into_buffer(&mut buffer);
+        let mut iter = 0;
+        assert_eq!(x, String::from_buffer(&buffer, &mut iter).unwrap());
+        assert_eq!(y, u16::from_buffer(&buffer, &mut iter).unwrap());
+        assert_eq!(z, String::from_buffer(&buffer, &mut iter).unwrap());
+        assert_eq!(Option::None, String::from_buffer(&buffer, &mut iter));
     }
 }
