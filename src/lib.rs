fd9c820a9392e419d91a38d683c6d231594ace1b
Cody Bloemhard <cblmhrd@live.nl>
2020-03-10 10:20:17 +0100

    f64 and f32 Bufferable

--- src/lib.rs
+++ src/lib.rs
@@ -70,6 +70,52 @@ impl Bufferable for u8{
     }
 }
 
+impl Bufferable for f64{
+    fn into_buffer(self, vec: &mut Buffer){
+        let bytes = self.to_be_bytes();
+        for b in bytes.iter(){
+            vec.push(*b);
+        }
+    }
+
+    fn copy_into_buffer(&self, vec: &mut Buffer){
+        self.clone().into_buffer(vec);
+    }
+
+    fn from_buffer(vec: &Buffer, iter: &mut usize) -> Option<Self>{
+        if *iter + 8 > vec.len() { return Option::None; }
+        let mut bytes = [0u8; 8];
+        for i in 0..8{
+            bytes[i] = vec[*iter + i];
+        }
+        *iter += 8;
+        return Option::Some(f64::from_be_bytes(bytes));
+    }
+}
+
+impl Bufferable for f32{
+    fn into_buffer(self, vec: &mut Buffer){
+        let bytes = self.to_be_bytes();
+        for b in bytes.iter(){
+            vec.push(*b);
+        }
+    }
+
+    fn copy_into_buffer(&self, vec: &mut Buffer){
+        self.clone().into_buffer(vec);
+    }
+
+    fn from_buffer(vec: &Buffer, iter: &mut usize) -> Option<Self>{
+        if *iter + 4 > vec.len() { return Option::None; }
+        let mut bytes = [0u8; 4];
+        for i in 0..4{
+            bytes[i] = vec[*iter + i];
+        }
+        *iter += 4;
+        return Option::Some(f32::from_be_bytes(bytes));
+    }
+}
+
 pub fn buffer_append_buffer(vec: &mut Buffer, string: &Buffer){
     for byte in string{
         vec.push(*byte);
@@ -140,5 +186,29 @@ mod tests{
         assert_eq!(x, u8::from_buffer(&buffer, &mut iter).unwrap());
         assert_eq!(y, u8::from_buffer(&buffer, &mut iter).unwrap());
         assert_eq!(Option::None, u8::from_buffer(&buffer, &mut iter));
+    }
+    #[test]
+    fn test_f64(){
+        let x = 1.001f64;
+        let y = 1.23456789;
+        let mut buffer = Vec::new();
+        x.into_buffer(&mut buffer);
+        y.into_buffer(&mut buffer);
+        let mut iter = 0;
+        assert_eq!(x, f64::from_buffer(&buffer, &mut iter).unwrap());
+        assert_eq!(y, f64::from_buffer(&buffer, &mut iter).unwrap());
+        assert_eq!(Option::None, f64::from_buffer(&buffer, &mut iter));
+    }
+    #[test]
+    fn test_f32(){
+        let x = 1.001f32;
+        let y = 1.23456;
+        let mut buffer = Vec::new();
+        x.into_buffer(&mut buffer);
+        y.into_buffer(&mut buffer);
+        let mut iter = 0;
+        assert_eq!(x, f32::from_buffer(&buffer, &mut iter).unwrap());
+        assert_eq!(y, f32::from_buffer(&buffer, &mut iter).unwrap());
+        assert_eq!(Option::None, f32::from_buffer(&buffer, &mut iter));
     }
 }
